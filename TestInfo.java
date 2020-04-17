0150951c361077a789af9452b2fd905f4f8ae27c
James Chapman <chapmanjamesg@gmail.com>
2020-04-17 14:17:22 -0500

    finished the first exercise of chapter 3

---
+++ TestInfo.java
@@ -0,0 +1,7 @@
+
+public class TestInfo {
+    public static void main(String[] args) {
+        System.out.println("Calling method from another class: ");
+        ParadiseInfo.displayInfo();
+    }
+}
