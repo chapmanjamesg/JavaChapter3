c541942b3f30292fe19b343d528ea366e61db31d
James Chapman <chapmanjamesg@gmail.com>
2020-04-20 09:33:41 -0500

    made my first class

---
+++ SpaService.java
@@ -0,0 +1,22 @@
+public class SpaService{
+
+    // Declaring data fields
+    private String serviceDescription;
+    private double price;
+    
+    // Making set methods
+    public void setServiceDescription(String service){
+        serviceDescription = service;
+    }
+    public void setPrice(double pr){
+        price = pr;
+    }
+    
+    // Making get methods
+    public String getServiceDescription(){
+        return serviceDescription;
+    }
+    public double getPrice(){
+        return price;
+    }
+}
